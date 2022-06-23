b216fa1ccd860308c60b6d69494b5c0c6a93bb19
ncaq <ncaq@ncaq.net>
2022-06-23 21:16:24 +0900

    fix: disable: footer-max-line-length

--- git-hooks/commitlint.config.js
+++ git-hooks/commitlint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    // URLやMarkdownのリンクなど改行出来ない要素が頻繁に頻繁に出現するため無効にする。
+    "footer-max-line-length": [0, "always"],
+  },
 };

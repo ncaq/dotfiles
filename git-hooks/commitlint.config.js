e2fbb898f6c8fd1932f7deefe5e2c024e1d34306
ncaq <ncaq@ncaq.net>
2022-06-24 18:29:02 +0900

    fix: disable: subject-case
    [subject-case rule breaks ConventionalCommits spec · Issue #2141 · conventional-changelog/commitlint](https://github.com/conventional-changelog/commitlint/issues/2141)

--- git-hooks/commitlint.config.js
+++ git-hooks/commitlint.config.js
@@ -3,5 +3,7 @@ module.exports = {
   rules: {
     // URLやMarkdownのリンクなど改行出来ない要素が頻繁に頻繁に出現するため無効にする。
     "footer-max-line-length": [0, "always"],
+    // 関数などの識別子などを直接コミットメッセージのタイトルに書きたいので無効にする。
+    "subject-case": [0, "always"],
   },
 };

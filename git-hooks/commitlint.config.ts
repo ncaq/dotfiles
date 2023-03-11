841ec844c5111bd264a4246ec68e3f5ec3171ebc
ncaq <ncaq@ncaq.net>
2023-03-11 17:30:07 +0900

    refactor: commitlint.configのTypeScript化

---
+++ git-hooks/commitlint.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-import-module-exports */
+import type { UserConfig } from "@commitlint/types";
+import { RuleConfigSeverity } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // URLやMarkdownのリンクなど改行出来ない要素が頻繁に頻繁に出現するため緩める。
+    "body-max-line-length": [RuleConfigSeverity.Disabled, "always", 200],
+    "footer-max-line-length": [RuleConfigSeverity.Disabled, "always", 200],
+    // 関数などの識別子などを直接コミットメッセージのタイトルに書きたいので無効にする。
+    "subject-case": [RuleConfigSeverity.Disabled, "always", []],
+  },
+};
+
+module.exports = Configuration;

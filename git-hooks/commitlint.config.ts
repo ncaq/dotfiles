de14e7ef0f5ceb268b2b7e2acf0f308188e8560f
ncaq <ncaq@ncaq.net>
2023-03-11 19:14:25 +0900

    refactor: disabledに設定を書くのは不要

--- git-hooks/commitlint.config.ts
+++ git-hooks/commitlint.config.ts
@@ -6,10 +6,10 @@ const Configuration: UserConfig = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     // URLやMarkdownのリンクなど改行出来ない要素が頻繁に頻繁に出現するため緩める。
-    "body-max-line-length": [RuleConfigSeverity.Disabled, "always", 200],
-    "footer-max-line-length": [RuleConfigSeverity.Disabled, "always", 200],
+    "body-max-line-length": [RuleConfigSeverity.Disabled],
+    "footer-max-line-length": [RuleConfigSeverity.Disabled],
     // 関数などの識別子などを直接コミットメッセージのタイトルに書きたいので無効にする。
-    "subject-case": [RuleConfigSeverity.Disabled, "always", []],
+    "subject-case": [RuleConfigSeverity.Disabled],
   },
 };
 

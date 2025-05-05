6133113e6a9f15015c258330ba7b391ff60afcd0
ncaq <ncaq@ncaq.net>
2025-05-06 03:02:39 +0900

    refactor: パッケージ管理を分割しレイヤー構成を整理

--- default.nix
+++ default.nix
@@ -6,58 +6,5 @@
 
   programs.home-manager.enable = true;
 
-  imports = [ ./link.nix ] ++ import ./programs { inherit builtins lib; };
-
-  home.packages = with pkgs; [
-    arandr
-    bash
-    bat
-    bat-extras.batdiff
-    bat-extras.batgrep
-    bat-extras.batman
-    bat-extras.batpipe
-    bat-extras.batwatch
-    bat-extras.prettybat
-    chromium
-    copyq
-    direnv
-    docker
-    docker-compose
-    emacs
-    fd
-    file
-    findutils
-    firefox
-    gimp
-    git
-    git-lfs
-    gnugrep
-    go
-    htop
-    inkscape
-    jq
-    kitty
-    less
-    libreoffice
-    lightdm
-    nano
-    obs-studio
-    pavucontrol
-    plocate
-    python3
-    rhythmbox
-    ripgrep
-    rsync
-    starship
-    thunderbird
-    trayer
-    tree
-    vlc
-    wget
-    xkeysnail
-    xorg.xrandr
-    xorg.xset
-    xsel
-    zsh
-  ];
+  imports = [ ./link.nix ] ++ import ./packages { inherit builtins lib; };
 }

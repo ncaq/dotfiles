dd81ced41442c198bd9b78dc7b28aa493fe3bcb5
ncaq <ncaq@ncaq.net>
2025-05-07 14:15:01 +0900

    feat: GNU Readlineの設定を宣言的に変更

---
+++ packages/readline.nix
@@ -0,0 +1,27 @@
+# GNU Readline の設定。
+# `.inputrc`の設定でもある。
+{ ... }:
+{
+  programs.readline = {
+    enable = true;
+
+    variables = {
+      editing-mode = "emacs";
+    };
+
+    bindings = {
+      # Controlキーバインド。
+      "\\C-b" = "backward-delete-char";
+      "\\C-h" = "backward-char";
+      "\\C-n" = "history-search-forward";
+      "\\C-s" = "forward-char";
+      "\\C-t" = "history-search-backward";
+
+      # Altキーバインド。
+      "\\eb" = "backward-kill-word";
+      "\\eh" = "backward-word";
+      "\\es" = "forward-word";
+      "\\et" = "history-search-backward";
+    };
+  };
+}

34f718dd6e6ab0a871be3b6f199733ae167002c6
ncaq <ncaq@ncaq.net>
2025-05-09 23:33:40 +0900

    feat: setting gui for WSL one host
    The adhoc.

--- home/package/xorg.nix
+++ home/package/xorg.nix
@@ -1,23 +1,11 @@
-{ pkgs, lib, ... }:
-let
-  # Path to the shell script
-  detectHiDpiScript = ./xorg-detect-hidpi.sh;
-in
+{ pkgs, ... }:
 {
-  # Install required packages
-  home.packages = (with pkgs; [ gawk ]) ++ (with pkgs.xorg; [ xrandr ]);
+  home.packages = with pkgs.xorg; [
+    xrdb
+  ];
 
-  # Dynamic DPI configuration logic using activation script
-  home.activation.detectAndSetDpi = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    export PATH="${pkgs.xorg.xrandr}/bin:${pkgs.gawk}/bin:$PATH"
-    # Auto-detect display resolution
-    if ${lib.getExe pkgs.bash} ${detectHiDpiScript}; then
-      $DRY_RUN_CMD echo "Xft.dpi: 144" > $HOME/.Xresources
-    else
-      # Use unlink instead of rm for safer operation
-      if [ -f "$HOME/.Xresources" ]; then
-        $DRY_RUN_CMD unlink "$HOME/.Xresources"
-      fi
-    fi
-  '';
+  # X リソースファイルを直接定義
+  xresources.properties = {
+    "Xft.dpi" = 144;
+  };
 }

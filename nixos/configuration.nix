8cdb122f80e042a509f6481212c82f17dbb4b13d
ncaq <ncaq@ncaq.net>
2025-05-13 19:07:37 +0900

    feat: add plocate

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  ...
+}:
 {
   system.stateVersion = "24.11";
 
@@ -25,7 +28,65 @@
     zsh.enable = true;
   };
 
-  services.dbus.packages = [ pkgs.dconf ];
+  services = {
+    dbus.packages = [ pkgs.dconf ];
+    locate = {
+      enable = true;
+      package = pkgs.plocate;
+      interval = "hourly";
+      pruneNames = [
+        "$Recycle.Bin"
+        ".Trash"
+        ".Trash-1000"
+        ".bzr"
+        ".cabal"
+        ".cache"
+        ".cargo"
+        ".dub"
+        ".gem"
+        ".ghcup"
+        ".git"
+        ".go"
+        ".hg"
+        ".julia"
+        ".metals"
+        ".npm"
+        ".pyenv"
+        ".rbenv"
+        ".rustup"
+        ".snapshots"
+        ".stack"
+        ".stack-work"
+        ".stack-work-fast"
+        ".stack-work-profile"
+        ".stack-work-test"
+        ".svn"
+        "WinSxS"
+        "__pycache__"
+        "_cache"
+        "_site"
+        "appcache"
+        "cache"
+        "cache2"
+        "cached"
+        "cdk.out"
+        "dist"
+        "dist-newstyle"
+        "dist-packages"
+        "eln-cache"
+        "elpy"
+        "file-backup"
+        "htmlcache"
+        "node_modules"
+        "site-packages"
+        "steam-runtime"
+        "target"
+        "texmf-dist"
+        "undo-tree"
+        "virtualenv"
+      ];
+    };
+  };
 
   users.users.ncaq = {
     isNormalUser = true;

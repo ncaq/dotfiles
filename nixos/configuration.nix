b201dc185e9a1b5475964fc845ee27f17c3241ef
ncaq <ncaq@ncaq.net>
2025-05-13 21:16:11 +0900

    refactor: separate `locate.nix` file

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,4 @@
-{
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 {
   system.stateVersion = "24.11";
 
@@ -30,62 +27,6 @@
 
   services = {
     dbus.packages = [ pkgs.dconf ];
-    locate = {
-      enable = true;
-      package = pkgs.plocate;
-      interval = "hourly";
-      pruneNames = [
-        "$Recycle.Bin"
-        ".Trash"
-        ".Trash-1000"
-        ".bzr"
-        ".cabal"
-        ".cache"
-        ".cargo"
-        ".dub"
-        ".gem"
-        ".ghcup"
-        ".git"
-        ".go"
-        ".hg"
-        ".julia"
-        ".metals"
-        ".npm"
-        ".pyenv"
-        ".rbenv"
-        ".rustup"
-        ".snapshots"
-        ".stack"
-        ".stack-work"
-        ".stack-work-fast"
-        ".stack-work-profile"
-        ".stack-work-test"
-        ".svn"
-        "WinSxS"
-        "__pycache__"
-        "_cache"
-        "_site"
-        "appcache"
-        "cache"
-        "cache2"
-        "cached"
-        "cdk.out"
-        "dist"
-        "dist-newstyle"
-        "dist-packages"
-        "eln-cache"
-        "elpy"
-        "file-backup"
-        "htmlcache"
-        "node_modules"
-        "site-packages"
-        "steam-runtime"
-        "target"
-        "texmf-dist"
-        "undo-tree"
-        "virtualenv"
-      ];
-    };
   };
 
   users.users.ncaq = {
@@ -95,4 +36,8 @@
     ];
     shell = pkgs.zsh;
   };
+
+  imports = [
+    ./locate.nix
+  ];
 }

a40796e76385dbc43207777638bb5dc3e1f219f9
ncaq <ncaq@ncaq.net>
2025-05-13 19:30:09 +0900

    feat: add mozc

---
+++ home/package/input-method.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+{
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-configtool
+      fcitx5-gtk
+      fcitx5-mozc-ut
+    ];
+  };
+
+  home.sessionVariables = {
+    GLFW_IM_MODULE = "ibus";
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    SDL_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+  };
+}

622ac9352a5cff19d6184f6217bb667880e227d6
ncaq <ncaq@ncaq.net>
2025-05-15 12:33:47 +0900

    refactor(package): move nix-prefetch to `nix.nix`

--- home/package/general.nix
+++ home/package/general.nix
@@ -13,7 +13,6 @@
     jq
     libwebp
     nano
-    nix-prefetch
     nkf
     opusTools
     oxipng
--- home/package/nix.nix
+++ home/package/nix.nix
@@ -1,10 +1,10 @@
-# Nix言語に関係するパッケージ。
-# Nixエコシステムまではあまりカバーしない。
+# Nix言語の編集・開発を補助するパッケージ。
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
     alejandra
     nil
+    nix-prefetch
     nixfmt-rfc-style
   ];
 }

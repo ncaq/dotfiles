c6a3e29e92dcf68c7e048671ed5b4cc4bfb0d03e
ncaq <ncaq@ncaq.net>
2025-05-17 11:05:22 +0900

    fix: Gentoo Linux上で動かすワークアラウンド
    * `PATH`などの環境変数がGentooだと特別扱いされているのでもう一度上書き
    * OpenGLを正常に設定するのが面倒なのでとりあえずホストのものを使う
    * `genericLinux`設定

--- home/package/kitty.nix
+++ home/package/kitty.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   programs.kitty = {
-    enable = true;
+    enable = false;
 
     settings = {
       font_size = 12;
--- home/package/path.nix
+++ home/package/path.nix
@@ -1,5 +1,6 @@
 { config, ... }:
 {
+  targets.genericLinux.enable = true;
   home.sessionPath = [
     "${config.home.homeDirectory}/.local/bin"
   ];

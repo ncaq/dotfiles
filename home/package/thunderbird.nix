db4a7166b9e01434ff4048ba02ae50c784e9ffd3
ncaq <ncaq@ncaq.net>
2025-05-25 19:52:43 +0900

    feat(package): Thunderbirdの設定ファイルを追加し、メールアカウントと各種プロファイル設定を導入

---
+++ home/package/thunderbird.nix
@@ -0,0 +1,51 @@
+{ ... }:
+{
+  programs.thunderbird = {
+    enable = true;
+    profiles.default = {
+      id = 0;
+      settings = {
+        "accessibility.typeaheadfind.manual" = false;
+        "browser.aboutConfig.showWarning" = false;
+        "browser.policies.applied" = true;
+        "browser.search.region" = "JP";
+        "browser.shell.didSkipDefaultBrowserCheckOnFirstRun" = true;
+        "calendar.timezone.local" = "Asia/Tokyo";
+        "calendar.ui.version" = 3;
+        "calendar.week.start" = 1; # 月曜始まり
+        "mail.shell.checkDefaultClient" = true;
+        "mail.showCondensedAddresses" = false; # 完全なアドレス表示
+        "mail.startup.enabledMailCheckOnce" = true;
+        "mail.uidensity" = 0; # コンパクト表示
+        "mailnews.default_news_sort_order" = 1;
+        "mailnews.default_sort_order" = 1; # 昇順ソート
+        "mailnews.message_display.disable_remote_image" = false; # リモート画像表示
+      };
+    };
+  };
+
+  accounts.email.accounts.main = {
+    address = "ncaq@ncaq.net";
+    userName = "ncaq.net@gmail.com";
+    realName = "ncaq";
+    primary = true;
+
+    imap = {
+      host = "imap.gmail.com";
+      port = 993;
+      tls.enable = true;
+    };
+
+    smtp = {
+      host = "smtp.gmail.com";
+      port = 465;
+      tls.enable = true;
+      tls.useStartTls = false;
+    };
+
+    thunderbird = {
+      enable = true;
+      profiles = [ "default" ];
+    };
+  };
+}

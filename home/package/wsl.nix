a638c28c33529288df80e287f24d86663d13ef93
ncaq <ncaq@ncaq.net>
2025-05-25 15:05:55 +0900

    fix: WSL環境判定に基づきwsl, xkeysnail, xmonadのインストール制御を追加
    wsl関連ツールはWSL環境でのみインストールし、
    xkeysnailやxmonadは非WSL環境でのみ有効化するように`lib.mkIf`で制御。
    これにより環境ごとの不要なパッケージインストールや競合を防止。

--- home/package/wsl.nix
+++ home/package/wsl.nix
@@ -1,5 +1,12 @@
-{ pkgs, ... }:
 {
+  pkgs,
+  lib,
+  isWSL,
+  ...
+}:
+# WSL関係のツールを普通のLinux環境にインストールしても、一見コンフリクトしなさそうに見えるが、
+# 実際使ってみるとGitHub CLIとかが検知して呼び出しを試みてしまう。
+lib.mkIf isWSL {
   home.packages = with pkgs; [
     wslu
   ];

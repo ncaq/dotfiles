fcf754e3d56e76bfd83f799781b1007f8663f6e9
ncaq <ncaq@ncaq.net>
2025-05-25 14:27:53 +0900

    feat(nixos): VirtualBox用ホスト `vanitas` 追加

---
+++ nixos/host/vanitas.nix
@@ -0,0 +1,35 @@
+{ ... }:
+{
+  # VirtualBoxのゲストOSを想定したNixOSのホスト定義。
+
+  # 普通の環境はEFIが有効だと思うので予行演習としてVirtualBoxのEFIサポートを有効にしている。
+  boot.loader = {
+    efi = {
+      efiSysMountPoint = "/boot";
+    };
+    grub = {
+      enable = true;
+      efiSupport = true;
+      devices = [ "nodev" ];
+    };
+  };
+  # VirtualBoxの標準的なデバイス名に合わせている。
+  # 普通はデバイス名はUUIDとかで設定するけど、
+  # そのへんの再現性が不明だし、
+  # 仮想環境だから雑に済ませている。
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/sda1";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+    };
+    "/" = {
+      device = "/dev/sda2";
+      fsType = "ext4";
+    };
+  };
+  virtualisation.virtualbox.guest.enable = true;
+}

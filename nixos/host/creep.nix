f26f4ee88de626a4392559f2941517de7b711404
ncaq <ncaq@ncaq.net>
2025-06-24 15:27:49 +0900

    feat(creep): add fileSystems configuration
    diskoは既存のWindowsファイルシステムを残す形だと使えないので、
    手動でラベル指定でパーティションを構築した後に読み込むことを想定する。

--- nixos/host/creep.nix
+++ nixos/host/creep.nix
@@ -11,4 +11,43 @@
       useOSProber = true;
     };
   };
+  fileSystems = {
+    "/boot/efi" = {
+      device = "/dev/disk/by-label/EFI";
+      fsType = "vfat";
+      options = [ "noatime" ];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/nixos-boot";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+    "/" = {
+      device = "/dev/disk/by-label/nixos-root";
+      fsType = "btrfs";
+      options = [
+        "noatime"
+        "compress=zstd"
+        "subvol=@"
+      ];
+    };
+    "/var/log" = {
+      device = "/dev/disk/by-label/nixos-root";
+      fsType = "btrfs";
+      options = [
+        "noatime"
+        "compress=zstd"
+        "subvol=@var-log"
+      ];
+    };
+    "/.snapshots" = {
+      device = "/dev/disk/by-label/nixos-root";
+      fsType = "btrfs";
+      options = [
+        "noatime"
+        "compress=zstd"
+        "subvol=@snapshots"
+      ];
+    };
+  };
 }

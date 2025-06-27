7c2b2cf4a6b448b2f9689f3e1ca987dc7d14d400
ncaq <ncaq@ncaq.net>
2025-06-27 12:53:17 +0900

    feat(creep): GoogleDriveをキャッシュする

--- home/package/rclone.nix
+++ home/package/rclone.nix
@@ -21,6 +21,12 @@ lib.mkIf (!isWSL) {
       mounts."GoogleDrive" = {
         enable = true;
         mountPoint = "${config.home.homeDirectory}/GoogleDrive";
+        # mount modeのデフォルトだとオフライン時に全くアクセス出来ないのでキャッシュする。
+        options = {
+          vfs-cache-mode = "full";
+          vfs-cache-max-age = "7d";
+          vfs-cache-max-size = "20G";
+        };
       };
     };
   };

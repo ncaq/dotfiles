e4ccc29d6122fd02c535dbde31aceb87f59224d5
ncaq <ncaq@ncaq.net>
2025-06-27 12:02:39 +0900

    feat(creep): grubではなくsystemd-bootを使うように変更
    `nixos-install`時に`/mnt/boot`にマウントポイントが存在する問題があり、
    システムデフォルトの方法を使うことを促されたため。
    もしかしたら`efiSysMountPoint`の設定をしてなかったからなだけなのかもしれないが、
    grubで出来ることが出来るならデフォルトから外れてこだわる意味はあまりない。

--- nixos/host/creep.nix
+++ nixos/host/creep.nix
@@ -8,13 +8,9 @@
     loader = {
       efi = {
         canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
       };
-      grub = {
-        enable = true;
-        devices = [ "nodev" ];
-        efiSupport = true;
-        useOSProber = true;
-      };
+      systemd-boot.enable = true;
     };
     initrd = {
       luks.devices = {

6a7c7d86551a52954feec0925423830302dd417a
ncaq <ncaq@ncaq.net>
2025-06-27 12:09:52 +0900

    feat: `nixos-generate-config --show-hardware-config --no-filesystems`
    インストーラの自動生成からNixOSネイティブ生成に切り替え。

--- nixos/host/creep/hardware-configuration.nix
+++ nixos/host/creep/hardware-configuration.nix
@@ -17,14 +17,10 @@
     "nvme"
     "xhci_pci"
     "thunderbolt"
-    "usb_storage"
-    "sd_mod"
   ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
-
-  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's

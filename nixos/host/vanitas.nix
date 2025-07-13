b68f083b33f48784940750701a294dba718b0375
ncaq <ncaq@ncaq.net>
2025-07-13 09:19:00 +0900

    fix(nixos): add native-linux import to creep and vanitas hosts
    `isWSL`の整理をしてた時にWSLじゃない方の処理を忘れていた。

--- nixos/host/vanitas.nix
+++ nixos/host/vanitas.nix
@@ -1,7 +1,11 @@
 { ... }:
 {
   # VirtualBoxのゲストOSを想定。
-  imports = [ ./vanitas/disk.nix ];
+  imports = [
+    ../native-linux
+
+    ./vanitas/disk.nix
+  ];
   # 普通の環境はEFIが有効だと思うので予行演習としてVirtualBoxのEFIサポートを有効にしている。
   boot = {
     loader = {

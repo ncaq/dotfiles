928dd8f4c329eb44e11d8e13a8f7c92ecd57104f
ncaq <ncaq@ncaq.net>
2025-08-13 19:22:45 +0900

    style(nixos/host): reorder imports for consistent module import order

--- nixos/host/bullet.nix
+++ nixos/host/bullet.nix
@@ -1,16 +1,16 @@
 { nixos-hardware, ... }:
 {
   imports = [
-    ../native-linux
-
-    ../desktop/dpms.nix
-
     nixos-hardware.nixosModules.common-cpu-amd
     nixos-hardware.nixosModules.common-cpu-amd-pstate
     nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
     nixos-hardware.nixosModules.common-hidpi
     nixos-hardware.nixosModules.common-pc
     nixos-hardware.nixosModules.common-pc-ssd
+
+    ../native-linux
+
+    ../desktop/dpms.nix
 
     ./bullet/boot.nix
     ./bullet/disk.nix

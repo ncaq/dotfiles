caf7bb8d248150e763efd9e938e4e37809abb3bc
ncaq <ncaq@ncaq.net>
2025-08-13 11:15:46 +0900

    feat(ssh): add authorized SSH key for configurable user

--- nixos/host/seminar/ssh.nix
+++ nixos/host/seminar/ssh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ username, ... }:
 {
   services.openssh = {
     enable = true;
@@ -8,4 +8,8 @@
     };
   };
   networking.firewall.allowedTCPPorts = [ 22 ];
+  users.users.${username}.openssh.authorizedKeys.keys = [
+    # 公開鍵は全世界に公開することが前提として設計されているので、dotfilesに含めて問題ない。
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICTOvA+ciR4NgQKH9yjQke+lMBhSK98VKrnBPRqt2BMt ncaq@bullet"
+  ];
 }

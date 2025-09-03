ef628183394bed03d09b21b1cf072349bf449165
ncaq <ncaq@ncaq.net>
2025-09-03 09:51:39 +0900

    feat(nixos): seminarホストにcloudflaredを追加

---
+++ nixos/host/seminar/cloudflare.nix
@@ -0,0 +1,19 @@
+{ username, ... }:
+{
+  # To initialize, run in server:
+  # ```
+  # nix run 'nixpkgs#cloudflared' -- tunnel login
+  # ```
+  # copy credentialsFile from terraform client to server.
+  services.cloudflared = {
+    enable = true;
+    certificateFile = "/home/${username}/.cloudflared/cert.pem";
+    tunnels.seminar = {
+      default = "http_status:404";
+      credentialsFile = "/home/${username}/.cloudflared/tunnel-seminar.json";
+      ingress = {
+        "seminar.ncaq.net" = "http://localhost:80";
+      };
+    };
+  };
+}

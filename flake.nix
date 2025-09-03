936cc3b031844859797b70b0f3bfb4c9756f11dd
ncaq <ncaq@ncaq.net>
2025-09-03 09:28:51 +0900

    chore(treefmt): treefmtでPrettierを有効化
    汎用的なフォーマッターとしてprettierを有効化。

--- flake.nix
+++ flake.nix
@@ -200,6 +200,7 @@
             programs = {
               deadnix.enable = true;
               nixfmt.enable = true;
+              prettier.enable = true;
               shellcheck.enable = true;
               shfmt.enable = true;
             };

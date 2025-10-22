abe4666233329aeec67cac24de19d179e0da244e
ncaq <ncaq@ncaq.net>
2025-10-22 15:47:04 +0900

    fix(grobi): outputs_connectedを使用して誤検出を防止
    `configure_single`だとIDとかの検査なしに検出してしまうようなので、
    `outputs_connected`を使うように変更しました。

--- home/package/grobi.nix
+++ home/package/grobi.nix
@@ -37,7 +37,6 @@ in
           "DP-2-GSM-23487-16843009-LG ULTRAGEAR+-"
           "DP-4-DEL-41599-810963027-AW2725Q-1JYC174"
         ];
-        # 凸型配置のためマニュアル設定。
         configure_command =
           "${pkgs.xorg.xrandr}/bin/xrandr"
           + " --output HDMI-0 --mode 3840x2160 --pos 3840x0"
@@ -92,25 +91,37 @@ in
       }
       {
         name = "dominaria-t"; # Top
-        configure_single = "HDMI-0-GSM-30470-699895-LG HDR 4K-";
+        atomic = true;
+        outputs_connected = [
+          "HDMI-0-GSM-30470-699895-LG HDR 4K-"
+        ];
         execute_after = dpiIs144;
       }
       {
         name = "dominaria-l"; # Left
-        configure_single = "DP-0-ACR-1680-2450570038-Acer VG270K-";
+        outputs_connected = [
+          "DP-0-ACR-1680-2450570038-Acer VG270K-"
+        ];
         execute_after = dpiIs144;
       }
       {
         name = "dominaria-r"; # Right
-        configure_single = "DP-2-GSM-23487-16843009-LG ULTRAGEAR+-";
+        outputs_connected = [
+          "DP-2-GSM-23487-16843009-LG ULTRAGEAR+-"
+        ];
         execute_after = dpiIs144;
       }
       {
         name = "dominaria-b"; # Bottom
-        configure_single = "DP-4-DEL-41599-810963027-AW2725Q-1JYC174";
+        outputs_connected = [
+          "DP-4-DEL-41599-810963027-AW2725Q-1JYC174"
+        ];
+        # Alienware AW2725Qは144Hzが最大ではないけれど4Kの場合144Hzが適切なのでマニュアル設定。
+        configure_command = "${pkgs.xorg.xrandr}/bin/xrandr --output DP-4 --mode 3840x2160 --rate 144 --primary";
         execute_after = dpiIs144;
       }
       {
+        # 主にラップトップ向けのfallback設定。
         name = "eDP-1";
         configure_single = "eDP-1";
       }

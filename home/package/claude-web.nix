2c196596cc69dc1a6061c84e7d045d5d87f7169d
ncaq <ncaq@ncaq.net>
2025-12-24 11:42:06 +0900

    docs: claude-web.nixのコメントをより具体的に修正
    Claudeが勘違いしたため。

--- home/package/claude-web.nix
+++ home/package/claude-web.nix
@@ -5,7 +5,7 @@
 {
   home.file = {
     # 通常版Claudeに必要なカスタムプロンプトを配置します。
-    # ここに配置する意味はまったくないのですが、
+    # ここに`CLAUDE_WEB.md`ファイルを配置する意味はまったくないのですが、
     # 公式に配置する場所がないので関連する場所ということで間借りさせてもらいます。
     "${config.xdg.configHome}/claude/CLAUDE_WEB.md".text = config.prompt.chat-assistant;
   };

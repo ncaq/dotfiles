d28593af42cf95c7d14b45d1c1b8cb31ca4079f3
ncaq <ncaq@ncaq.net>
2026-01-04 12:13:19 +0900

    feat(tailscale): useRoutingFeaturesを"server"から"both"に変更
    seminar側でもtailscaleの機能使うかもしれないと思ったので。
    
    - Exit Nodeとしてクライアント機能も有効化
    - ルーティング機能の拡張により柔軟な通信が可能

--- nixos/host/seminar/tailscale.nix
+++ nixos/host/seminar/tailscale.nix
@@ -2,7 +2,7 @@
 {
   # Exit Nodeとして動作するための追加設定。
   # 基本的なTailscale有効化は nixos/core/tailscale.nix で行っている。
-  services.tailscale.useRoutingFeatures = "server";
+  services.tailscale.useRoutingFeatures = "both";
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = 1;
     "net.ipv6.conf.all.forwarding" = 1;

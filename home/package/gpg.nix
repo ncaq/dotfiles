b6b60aaa9c4b9b7eb578a58191891e6b6b24a1e5
ncaq <ncaq@ncaq.net>
2026-01-05 14:27:00 +0900

    feat(gpg): pinentryにpinentry-gnome3を指定する
    GUIアプリ触っている時にパスフレーズを求められたときは役に立つらしい。

--- home/package/gpg.nix
+++ home/package/gpg.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.gpg.enable = true;
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
+    pinentry.package = pkgs.pinentry-gnome3;
   };
 }

cd09f6438fad00f0511c858338c268ac713754ee
ncaq <ncaq@ncaq.net>
2026-01-10 12:47:39 +0900

    feat(package): prefetch系ツールを追加して取得対応を拡張
    - nix-prefetch-bzr, nix-prefetch-cvs, nix-prefetch-docker,
      nix-prefetch-hg, nix-prefetch-svn, prefetch-npm-deps,
      prefetch-yarn-depsをhome.packagesに追加
    - より多様なソース取得に対応

--- home/package/prefetch.nix
+++ home/package/prefetch.nix
@@ -2,7 +2,14 @@
 {
   home.packages = with pkgs; [
     nix-prefetch
+    nix-prefetch-bzr
+    nix-prefetch-cvs
+    nix-prefetch-docker
     nix-prefetch-git
     nix-prefetch-github
+    nix-prefetch-hg
+    nix-prefetch-svn
+    prefetch-npm-deps
+    prefetch-yarn-deps
   ];
 }

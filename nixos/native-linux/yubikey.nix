a1112d3bbe8869821724a4ff62621215c715e83f
ncaq <ncaq@ncaq.net>
2026-01-15 12:26:56 +0900

    refactor(yubikey): 設定コメントとパッケージ記述を改善
    セキュリティキーとしては使える可能性が出てきたため。
    
    - GPG利用状況の説明をコメントで明確化
    - pcscdのコメントをより分かりやすく修正
    - systemPackagesにyubikey-personalizationを追加

--- nixos/native-linux/yubikey.nix
+++ nixos/native-linux/yubikey.nix
@@ -1,15 +1,16 @@
 { pkgs, ... }:
 {
   # YubiKeyは一応持ってるので使えるように設定していますが、
-  # ファームウェアのバージョンが現状古くて楕円曲線暗号に対応していないため、
-  # 今はあまり使っていません。
+  # ファームウェアのバージョンが5.1.2で古くて楕円曲線暗号に対応していないため、
+  # 現状GPGには使えていません。
 
   services = {
-    pcscd.enable = true; # GPGなどと通信をします。
+    pcscd.enable = true; # GPGなどと通信が出来るように。
     udev.packages = [ pkgs.yubikey-personalization ];
   };
 
   environment.systemPackages = with pkgs; [
     yubikey-manager # ykman コマンド - YubiKeyの状態確認やPIN管理に便利。
+    yubikey-personalization # よりEasyなyubikeyの設定ツール。
   ];
 }

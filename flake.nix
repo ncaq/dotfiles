7d762e9853c8c7012c7fc310921c8bd8347c389f
ncaq <ncaq@ncaq.net>
2026-01-18 10:42:38 +0900

    ci(treefmt): actionlintを有効化

--- flake.nix
+++ flake.nix
@@ -274,6 +274,7 @@
           treefmt.config = {
             projectRootFile = "flake.nix";
             programs = {
+              actionlint.enable = true;
               deadnix.enable = true;
               nixfmt.enable = true;
               prettier.enable = true;

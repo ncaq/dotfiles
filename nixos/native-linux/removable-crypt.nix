7666b1f4c859ab2ca9e5dca68d9f93910867204d
ncaq <ncaq@ncaq.net>
2026-01-22 14:35:45 +0900

    feat(removable-crypt): gideonデバイスIDを追加

--- nixos/native-linux/removable-crypt.nix
+++ nixos/native-linux/removable-crypt.nix
@@ -4,6 +4,7 @@
   programs.removable-crypt = {
     enable = true;
     devices = {
+      gideon.deviceId = "usb-USB_SanDisk_3.2Gen1_03005417112725151804-0:0-part1";
       two-thousand.deviceId = "usb-JetFlash_Transcend_32GB_25XSK57XTBIHQODC-0:0-part1";
     };
   };

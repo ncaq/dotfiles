5017379f329d186a19cddd89a51bad7beaffd091
ncaq <ncaq@ncaq.net>
2026-01-22 16:47:32 +0900

    feat(filesystem): ファイルシステム関連パッケージを追加
    btrfs-progsは元から暗黙的に使えていましたが、
    明示的にインストールします。
    
    - btrfs-progs, exfat, exfatprogs, partedをhome.packagesに追加
    - より多様なファイルシステム操作に対応

--- home/package/filesystem.nix
+++ home/package/filesystem.nix
@@ -1,10 +1,14 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    btrfs-progs
     cryptsetup
     duperemove
+    exfat
+    exfatprogs
     gnome.gvfs
     ntfs3g
+    parted
     squashfsTools
   ];
 }

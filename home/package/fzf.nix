df57249a066b5af86ff1fb949dce872f7c248be3
ncaq <ncaq@ncaq.net>
2026-01-31 23:34:52 +0900

    feat(fzf): fzfオプションを順当に改善
    - fdによるファイル/ディレクトリ検索に変更
    - bat/ treeによるプレビュー機能を追加
    - --reverse, --border等UI改善オプションを追加
    - 履歴検索に--exactを追加

--- home/package/fzf.nix
+++ home/package/fzf.nix
@@ -1,5 +1,4 @@
-{ ... }:
-{
+_: {
   programs.fzf = {
     enable = true;
 
@@ -13,6 +12,28 @@
       "--bind=ctrl-b:backward-delete-char"
       "--bind=alt-h:backward-word,alt-s:forward-word"
       "--bind=alt-b:backward-kill-word"
+      "--reverse"
+      "--border"
+    ];
+
+    # findの代わりにfdを使う
+    defaultCommand = "fd --type f --hidden --exclude .git";
+
+    # ファイル選択(batでプレビュー)
+    fileWidgetCommand = "fd --type f --hidden --exclude .git";
+    fileWidgetOptions = [
+      "--preview 'bat --color=always --style=numbers --line-range=:500 {}'"
+    ];
+
+    # ディレクトリ移動(treeでプレビュー)
+    changeDirWidgetCommand = "fd --type d --hidden --exclude .git";
+    changeDirWidgetOptions = [
+      "--preview 'tree -C -L 2 {}'"
+    ];
+
+    # 履歴検索
+    historyWidgetOptions = [
+      "--exact"
     ];
   };
 }

fd7731c7326e49a864187ace1898146c783893db
ncaq <ncaq@ncaq.net>
2026-02-03 08:18:29 +0900

    feat(home): nix-on-droid用の初期設定ファイルを追加
    - Android端末向けにetcバックアップ拡張子を.bakに設定
    - タイムゾーンをAsia/Tokyoに明示的に指定

---
+++ home/nix-on-droid.nix
@@ -0,0 +1,9 @@
+_: {
+  # nix-on-droidのデフォルト設定。
+  # Android端末でコンフリクトしたファイルを処理するのには手間がかかるので合理的。
+  environment.etcBackupExtension = ".bak";
+
+  # Androidホストのタイムゾーンは自動的に引き継がれないようなので、
+  # 明示的に設定。
+  time.timeZone = "Asia/Tokyo";
+}

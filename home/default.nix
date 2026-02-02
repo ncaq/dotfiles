baec42186b08252b79142fc88d4939f8ee17dc96
ncaq <ncaq@ncaq.net>
2026-02-03 08:17:04 +0900

    fix(home): home-manager環境でnixのexperimental-featuresを明示指定
    - home-manager環境でnixosの設定が反映されない場合に備え、
      nix.extraOptionsでexperimental-featuresを明示的に指定
    - flakesとnix-commandが常に有効になるように設定

--- home/default.nix
+++ home/default.nix
@@ -6,9 +6,15 @@
 {
   home.username = username;
   home.homeDirectory = "/home/${config.home.username}";
-  home.stateVersion = "25.05";
 
+  home.stateVersion = "25.05";
   programs.home-manager.enable = true;
+
+  # home-manager環境だとnixosの設定が効いてないので有効になっていないことがあるので、
+  # 明示的に再度指定。
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
 
   imports = [
     ./link.nix

a46f6d575b2f7b14bb3f165f3c839c612140e4a3
ncaq <ncaq@ncaq.net>
2026-02-03 08:24:05 +0900

    feat(home): home-managerの方にもlocale設定を明示的に追加
    - i18n.glibcLocalesに"ja_JP.UTF-8" "en_US.UTF-8" "C.UTF-8"を追加
    - 多言語環境や一部ツールの動作安定性向上のため

--- home/default.nix
+++ home/default.nix
@@ -8,6 +8,13 @@
   home.homeDirectory = "/home/${config.home.username}";
 
   home.stateVersion = "25.05";
+
+  i18n.glibcLocales = [
+    "ja_JP.UTF-8"
+    "en_US.UTF-8"
+    "C.UTF-8"
+  ];
+
   programs.home-manager.enable = true;
 
   # home-manager環境だとnixosの設定が効いてないので有効になっていないことがあるので、

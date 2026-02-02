b934245019e1642f460e8ac4f2788fb83bbbee74
ncaq <ncaq@ncaq.net>
2026-02-03 07:07:28 +0900

    fix(zsh): tmux起動時にnixのパスを利用するように変更
    条件式も入れて、
    存在しないコマンドを実行してしまうリスクを極力なくす。

--- home/package/zsh.nix
+++ home/package/zsh.nix
@@ -14,8 +14,8 @@ in
 
       # Nixの自動生成するものではないユーザのzshrcを読み込む。
       initContent = ''
-        if [[ -z "$TMUX" ]] && [[ $- == *i* ]]; then
-          exec tmux new -A -s master
+        if [ -x "$(command -v ${lib.getExe pkgs.tmux})" ] && [[ -z "$TMUX" ]] && [[ $- == *i* ]]; then
+          exec ${lib.getExe pkgs.tmux} new -A -s master
         fi
         if [ -f "${zshDotDir}/.zshrc" ]; then
           source "${zshDotDir}/.zshrc"

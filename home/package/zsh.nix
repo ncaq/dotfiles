cb438640fcb58d0eb0ceb99e5a05d2ada92e42c3
ncaq <ncaq@ncaq.net>
2026-02-03 12:01:26 +0900

    refactor(zsh): tmux起動判定の条件分岐を反転し分岐を読みやすくする

--- home/package/zsh.nix
+++ home/package/zsh.nix
@@ -15,11 +15,11 @@ in
       # Nixの自動生成するものではないユーザのzshrcを読み込む。
       initContent = ''
         if [[ -x "$(command -v ${lib.getExe pkgs.tmux})" ]] && [[ -z "$TMUX" ]] && [[ $- == *i* ]]; then
-          if ! ${lib.getExe pkgs.tmux} new -A -s master; then
-            echo "Warning: tmux failed to start. Falling back to normal shell." >&2
-          else
+          if ${lib.getExe pkgs.tmux} new -A -s master; then
             # tmuxセッションが正常終了した場合、zshシェルも終了します。
             exit $?
+          else
+            echo "Warning: tmux failed to start. Falling back to normal shell." >&2
           fi
         fi
         if [ -f "${zshDotDir}/.zshrc" ]; then

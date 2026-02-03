e1ca62de7f7462933eeaf1a1be6ee49e63bcaa17
ncaq <ncaq@ncaq.net>
2026-02-03 12:55:08 +0900

    feat(install): Termux環境でnix-on-droidを使う分岐を追加
    - TERMUX_VERSIONが設定されている場合にnix-on-droidを利用するよう修正
    - NixOS/Termux/その他で適切なコマンドを選択するように分岐を整理

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 if [ -f /etc/NIXOS ]; then
   sudo nixos-rebuild switch --flake ".#$(hostname)"
+elif [ -n "${TERMUX_VERSION:-}" ]; then
+  nix-on-droid switch --flake "."
 else
   home-manager --flake ".#${USER}" switch
 fi

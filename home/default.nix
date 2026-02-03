b4b5cdd826d03ad654bf032f96fc71c134ed6b26
ncaq <ncaq@ncaq.net>
2026-02-03 13:22:40 +0900

    fix(home): nix.extraOptionsの設定を削除
    Nix-on-Droidの指定で設定したが、
    設定するとエラーになるらしい。

--- home/default.nix
+++ home/default.nix
@@ -25,12 +25,6 @@
 
   programs.home-manager.enable = true;
 
-  # home-manager環境だとnixosの設定が効いてないので有効になっていないことがあるので、
-  # 明示的に再度指定。
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
-
   imports = [
     ./link.nix
     ./prompt

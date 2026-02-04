0cf10f1346538f605466337c2a07c8d684ea8614
ncaq <ncaq@ncaq.net>
2026-02-04 13:16:01 +0900

    build(flake): flake.nix側のキャッシュサーバ利用設定を削除
    トークンが必ず必要なキャッシュサーバをリポジトリ側の設定で利用するように書くのはトラブルを招くため。
    そういうのはCIの設定側などでやるべき。

--- flake.nix
+++ flake.nix
@@ -367,14 +367,12 @@
       "https://nix-community.cachix.org/"
       "https://cache.iog.io/"
       "https://ncaq-dotfiles.cachix.org/"
-      "https://seminar.border-saurolophus.ts.net/nix/cache/private/"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "ncaq-dotfiles.cachix.org-1:oEM1SL5sNteDM16I23/rFZwKl+Anca/PnEWp6LWUrws="
-      "private:7SHPFhFIAzd4F78w1+EXgwP0HKApgLBbu9IJG8GE2I4="
     ];
   };
 }

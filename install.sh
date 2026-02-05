e1c86f334bcfb2fb76a3eb45e9888d4cd5ba3a87
ncaq <ncaq@ncaq.net>
2026-02-05 15:12:44 +0900

    style(install.sh): インデントを修正して可読性を向上
    `nix fmt`

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ if [ -f /etc/NIXOS ]; then
   sudo nixos-rebuild switch --flake ".#$(hostname)"
 else
   case $(uname -m) in
-    x86_64)
-      home-manager --flake ".#x86_64-linux" switch
-      ;;
-    aarch64)
-      home-manager --flake ".#aarch64-linux" switch
-      ;;
-    *)
-      echo "その他のアーキテクチャ: $(uname -m)"
-      ;;
+  x86_64)
+    home-manager --flake ".#x86_64-linux" switch
+    ;;
+  aarch64)
+    home-manager --flake ".#aarch64-linux" switch
+    ;;
+  *)
+    echo "その他のアーキテクチャ: $(uname -m)"
+    ;;
   esac
 fi

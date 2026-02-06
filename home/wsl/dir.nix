65ea3b525dd723e2a4fcb3a48e98a1aa29eadd7f
ncaq <ncaq@ncaq.net>
2026-02-06 17:22:41 +0900

    refactor(xdg): WSL用ディレクトリ設定をhome/wsl/dir.nixへ分離
    - WSL環境向けのVideos, GoogleDrive, WinHome, WinDownloadsの
      シンボリックリンク設定をxdg.nixからdir.nixへ移動
    - xdg.nixは共通設定のみに簡素化し、WSL固有処理を分離
    - 設定の責務を明確化し、保守性を向上

---
+++ home/wsl/dir.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  username,
+  ...
+}:
+let
+  windowsUsername = username;
+  WindowsUserHome = "/mnt/c/Users/${windowsUsername}";
+in
+{
+  home.file = {
+    # WindowsのHDD側を参照。
+    "Videos" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/mnt/d/Videos/";
+    };
+    # Windowsホストの管理するGoogle Driveディレクトリを参照。
+    "GoogleDrive" = {
+      source = config.lib.file.mkOutOfStoreSymlink (WindowsUserHome + "/マイドライブ");
+    };
+    # WSLで便利なリンク。
+    "WinHome" = {
+      source = config.lib.file.mkOutOfStoreSymlink WindowsUserHome;
+    };
+    # WSLで便利なリンク。
+    "WinDownloads" = {
+      source = config.lib.file.mkOutOfStoreSymlink (WindowsUserHome + "/Downloads");
+    };
+  };
+}

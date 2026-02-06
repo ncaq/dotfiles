2096c1978048abd155afe381744326847004e52b
ncaq <ncaq@ncaq.net>
2026-02-06 17:56:36 +0900

    feat(wsl): WindowsのVideosディレクトリへのSMBリンクを追加
    - Windows側のSMB共有を利用したVideosディレクトリのシンボリックリンクを追加
    - 利用方法のコメントを記載

--- home/wsl/dir.nix
+++ home/wsl/dir.nix
@@ -13,6 +13,14 @@ in
     "GoogleDrive" = {
       source = config.lib.file.mkOutOfStoreSymlink (windowsUserHome + "/マイドライブ");
     };
+    # Windows側のSMBで解決。
+    "Videos" = {
+      # 以下のようにWindows側でコマンドを使って共有ディレクトリをマウントしてください。
+      # ```
+      # net use S: \\SEMINAR\chihiro /persistent:yes
+      # ```
+      source = config.lib.file.mkOutOfStoreSymlink "/mnt/s/Videos";
+    };
     # WSLで便利なリンク。
     "WinHome" = {
       source = config.lib.file.mkOutOfStoreSymlink windowsUserHome;

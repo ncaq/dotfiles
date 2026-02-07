708055989ab0fb3cd0d7a4260c3da248434da8df
ncaq <ncaq@ncaq.net>
2026-02-07 18:00:38 +0900

    fix(attic-init): attic loginを引数で行います
    Nix-on-Droidで何故かうまくいかないので。
    引数を利用することで一瞬`ps`でトークンが見れる問題は、
    セキュリティ上無視できる問題だと判断します。
    そもそも`ps`を実行される権限があるのが問題外と考えます。

--- home/core/attic-init.nix
+++ home/core/attic-init.nix
@@ -16,6 +16,11 @@
   };
 
   # インストール時にキャッシュ設定を初期化します。
+  # Nix-on-Droidで何故かうまくいかないので、
+  # パイプではなく引数を使っています。
+  # 引数を利用することで一瞬`ps`でトークンが見れる問題は、
+  # セキュリティ上無視できる問題だと判断します。
+  # そもそも`ps`を実行される権限があるのが問題外と考えます。
   home.activation.attic-init = lib.hm.dag.entryAfter [ "sops-nix" ] ''
     if [[ -f "${config.sops.secrets."attic-token".path}" ]]; then
       # サーバーへの接続確認
@@ -23,8 +28,10 @@
          --head --silent --fail \
          --connect-timeout 5 --max-time 10 \
          https://seminar.border-saurolophus.ts.net/nix/cache/; then
-        $DRY_RUN_CMD ${pkgs.coreutils}/bin/cat ${config.sops.secrets."attic-token".path} | \
-          ${pkgs.attic-client}/bin/attic login ncaq https://seminar.border-saurolophus.ts.net/nix/cache/
+        token=$(${pkgs.coreutils}/bin/cat ${config.sops.secrets."attic-token".path})
+        $DRY_RUN_CMD ${pkgs.attic-client}/bin/attic login ncaq \
+          https://seminar.border-saurolophus.ts.net/nix/cache/ \
+          "$token"
         $DRY_RUN_CMD ${pkgs.attic-client}/bin/attic use ncaq:private
       else
         echo "attic-init: サーバーに接続できないためスキップします"

0cbcd83711e9c3ce92eb9a3988d03282b65a74ad
ncaq <ncaq@ncaq.net>
2026-02-07 17:34:25 +0900

    fix(ssh): opensshパッケージを明示的に指定して一貫性を確保
    - システムにopensshがない場合でも確実にインストールされるよう修正
    - `programs.ssh.package`に`pkgs.openssh`を指定

--- home/core/ssh.nix
+++ home/core/ssh.nix
@@ -1,6 +1,8 @@
-_: {
+{ pkgs, ... }:
+{
   programs.ssh = {
     enable = true;
+    package = pkgs.openssh; # システムのがない場合に備えて一貫してインストール指定。
     enableDefaultConfig = false;
     matchBlocks = {
       "ssh.forgejo.ncaq.net" = {

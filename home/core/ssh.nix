39beaed49f4d8cb660205e61729460d7129fe3ed
ncaq <ncaq@ncaq.net>
2026-02-07 22:34:42 +0900

    feat(ssh): seminarとforgejo用のユーザー設定を追加
    - seminarホストにuser "ncaq"を追加
    - ssh.forgejo.ncaq.netにuser "forgejo"を追加

--- home/core/ssh.nix
+++ home/core/ssh.nix
@@ -5,8 +5,12 @@
     package = pkgs.openssh; # システムのがない場合に備えて一貫してインストール指定。
     enableDefaultConfig = false;
     matchBlocks = {
+      "seminar" = {
+        user = "ncaq";
+      };
       "ssh.forgejo.ncaq.net" = {
         port = 2222;
+        user = "forgejo";
         extraOptions = {
           # Forgejo's built-in SSH server doesn't support post-quantum key exchange.
           WarnWeakCrypto = "no-pq-kex";

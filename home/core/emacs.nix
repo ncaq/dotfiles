7feef7174b37e111dfd1a5260b09a068829e205c
ncaq <ncaq@ncaq.net>
2026-02-10 12:15:21 +0900

    docs(emacs): Emacs設定管理方針のコメントを追加

--- home/core/emacs.nix
+++ home/core/emacs.nix
@@ -15,6 +15,13 @@
     defaultEditor = true;
   };
 
+  # Emacsの設定はEmacs Lispで行うのがDSLとして最適化されていて楽なので、
+  # Nix言語ではなく`.emacs.d`で基本的に管理します。
+  # またEmacsの設定は即座に反映したいため、
+  # currentとしてはinputs頼りではなくcloneしたものを直接参照します。
+  # inputsにも入れていますが、
+  # それは外部ライブラリを自動的に入れるためのもので、
+  # あくまでショートカットです。
   home.activation.cloneEmacsConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if [ ! -d "${config.home.homeDirectory}/.emacs.d" ]; then
       $DRY_RUN_CMD ${pkgs.git}/bin/git clone \

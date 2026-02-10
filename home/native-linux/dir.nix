92984c82a3c4032c8792355e7169cf0c62e75566
ncaq <ncaq@ncaq.net>
2026-02-10 10:53:56 +0900

    fix: CIFSによるファイルシステム設定を削除
    意味が分からないのですが、
    インストールして再起動して、
    もう一度インストールして再起動するとハングします。
    noautoオプションをつけているので自動マウントはされないはずなのですが。
    早急にsambaのマウントが必要なわけではないので、
    ひとまずCIFSによるマウント設定を削除します。

--- home/native-linux/dir.nix
+++
@@ -1,9 +0,0 @@
-{ config, ... }:
-{
-  home.file = {
-    "Videos" = {
-      # 既にcifsでマウントされている自宅サーバのVideosディレクトリを参照。
-      source = config.lib.file.mkOutOfStoreSymlink "/mnt/chihiro/Videos";
-    };
-  };
-}

17d2fcbe984ff31324a75e761c3df8ea99f70374
ncaq <ncaq@ncaq.net>
2026-02-11 13:16:21 +0900

    feat(boot): LUKS解錠でFIDO2を試行
    バックアップとして普段は使わない長いリカバリーキーも別スロットに入れています。
    本当は中間にtpm2のpinも挟みたかったのですが、
    現状はtpm2のスロットが存在するだけでfido2より優先してプロンプトが出てしまうため、
    tpm2による中間のフォールバックは諦めています。
    
    - initrdでsystemdを有効化
    - LUKSデバイスのcrypttabExtraOptsにfido2-device=auto追加

--- nixos/host/creep/boot.nix
+++ nixos/host/creep/boot.nix
@@ -13,10 +13,14 @@
       };
     };
     initrd = {
+      systemd.enable = true;
       luks.devices = {
         nixos-root = {
           device = "/dev/disk/by-label/nixos-root-crypt";
           allowDiscards = true;
+          crypttabExtraOpts = [
+            "fido2-device=auto"
+          ];
         };
       };
     };

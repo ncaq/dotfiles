3ddde5cfabcf59609e8612605ebd97119d2c5457
ncaq <ncaq@ncaq.net>
2026-02-15 18:51:47 +0900

    refactor(host): imports定義をwith構文で簡素化

--- nixos/host/bullet.nix
+++ nixos/host/bullet.nix
@@ -1,12 +1,12 @@
 { inputs, ... }:
 {
-  imports = [
-    inputs.nixos-hardware.nixosModules.common-cpu-amd
-    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-    inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
-    inputs.nixos-hardware.nixosModules.common-hidpi
-    inputs.nixos-hardware.nixosModules.common-pc
-    inputs.nixos-hardware.nixosModules.common-pc-ssd
+  imports = with inputs; [
+    nixos-hardware.nixosModules.common-cpu-amd
+    nixos-hardware.nixosModules.common-cpu-amd-pstate
+    nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
+    nixos-hardware.nixosModules.common-hidpi
+    nixos-hardware.nixosModules.common-pc
+    nixos-hardware.nixosModules.common-pc-ssd
 
     ../native-linux
 

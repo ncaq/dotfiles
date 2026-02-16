f05c1feb2a6d8ef48160bca7d3c61fb43839b47c
ncaq <ncaq@ncaq.net>
2026-02-16 12:41:47 +0900

    feat(core): 各種ツール群に新規パッケージを追加
    - audio: flacを追加
    - compress: bzip2, unrar-free, unzip, xz, zlib, zstdを追加
    - image: imagemagick, mozjpegを追加
    - video: svt-av1を追加

--- home/core/compress.nix
+++ home/core/compress.nix
@@ -1,7 +1,13 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    bzip2
     p7zip
     patool
+    unrar-free
+    unzip
+    xz
+    zlib
+    zstd
   ];
 }
--- home/core/image.nix
+++ home/core/image.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    imagemagick
     libwebp
+    mozjpeg
     oxipng
   ];
 }

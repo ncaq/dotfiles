4bfd36e40cac1c43744e9385ac162cfa22c07251
ncaq <ncaq@ncaq.net>
2026-02-17 12:03:35 +0900

    style(home): home属性のネストを整理し可読性を向上

--- home/default.nix
+++ home/default.nix
@@ -7,13 +7,15 @@
   ...
 }:
 {
-  home.stateVersion = "25.05";
+  home = {
+    stateVersion = "25.05";
 
-  # ユーザ名の制約が強い環境で譲ります。
-  home.username = lib.mkDefault username;
+    # ユーザ名の制約が強い環境で譲ります。
+    username = lib.mkDefault username;
 
-  # ホームディレクトリはプラットフォームの制約が強いのでプラットフォームの設定を尊重します。
-  home.homeDirectory = lib.mkDefault "/home/${config.home.username}";
+    # ホームディレクトリはプラットフォームの制約が強いのでプラットフォームの設定を尊重します。
+    homeDirectory = lib.mkDefault "/home/${config.home.username}";
+  };
 
   programs.home-manager.enable = true;
 

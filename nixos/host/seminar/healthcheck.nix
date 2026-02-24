da47fd52b408a33f4548b486807bab4d73a1c71a
ncaq <ncaq@ncaq.net>
2026-02-24 17:44:26 +0900

    fix(seminar/healthcheck): healthcheckユーザー用runuserのPAM設定を削除
    どうにも中途半端でうまくいかないので一回諦めることにした。
    ログに出てくるだけですし。

--- nixos/host/seminar/healthcheck.nix
+++ nixos/host/seminar/healthcheck.nix
@@ -8,20 +8,4 @@
     };
     groups.healthcheck = { };
   };
-  # runuserのPAM設定: healthcheckユーザーの場合はpam_unix.soのsessionをスキップしてログ抑制
-  security.pam.services.runuser.rules.session = {
-    # healthcheckユーザーの場合は次のunixルールをスキップ
-    skip-unix-for-healthcheck = {
-      order = config.security.pam.services.runuser.rules.session.unix.order - 1;
-      control = "[success=1 default=ignore]";
-      modulePath = "${pkgs.pam}/lib/security/pam_succeed_if.so";
-      args = [
-        "quiet"
-        "user"
-        "="
-        "healthcheck"
-      ];
-    };
-    # 既存のunixルールはorder=10200で実行される(healthcheck以外のユーザーのみ)
-  };
 }

3c082bc13cf2ecef7ccc324fceaef5a433376a8d
ncaq <ncaq@ncaq.net>
2026-02-24 10:54:23 +0900

    refactor(nix): nix-fast-buildラッパーの実行方法を改善
    パスの順序に依存しない呼び出し方を使う。

--- home/core/nix.nix
+++ home/core/nix.nix
@@ -4,9 +4,8 @@ let
   # nix-fast-buildのラッパー: SQLiteキャッシュの競合のエラーを回避するために評価キャッシュを無効化しています。
   nix-fast-build-wrapper = pkgs.writeShellApplication {
     name = "nix-fast-build";
-    runtimeInputs = [ pkgs.nix-fast-build ];
     text = ''
-      exec nix-fast-build --option eval-cache false "$@"
+      exec ${pkgs.lib.getExe pkgs.nix-fast-build} --option eval-cache false "$@"
     '';
   };
 in

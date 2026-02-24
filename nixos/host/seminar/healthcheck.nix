2eff9afb8d3eb6c5724382bcca7630d85ccae1a8
ncaq <ncaq@ncaq.net>
2026-02-24 17:26:57 +0900

    fix(seminar): healthcheckユーザーのPAM設定とmackerel監視改善
    - healthcheckユーザーのrunuser時にセッションログを抑制するPAM設定を追加
    - healthcheckユーザーの監視実行時のログ出力を最小化

--- nixos/host/seminar/healthcheck.nix
+++ nixos/host/seminar/healthcheck.nix
@@ -1,9 +1,33 @@
-_:
+{ pkgs, config, ... }:
 {
   # ヘルスチェック用システムユーザー
-  users.users.healthcheck = {
-    isSystemUser = true;
-    group = "healthcheck";
+  users = {
+    users.healthcheck = {
+      isSystemUser = true;
+      group = "healthcheck";
+    };
+    groups.healthcheck = { };
   };
-  users.groups.healthcheck = { };
+  # runuserのPAM設定: healthcheckユーザーの場合のみセッションログを抑制
+  security.pam.services.runuser.rules.session = {
+    # healthcheckユーザーかチェック
+    check-healthcheck-user = {
+      order = config.security.pam.services.runuser.rules.session.unix.order - 1;
+      control = "[success=1 default=ignore]";
+      modulePath = "${pkgs.pam}/lib/security/pam_succeed_if.so";
+      args = [
+        "quiet"
+        "user"
+        "="
+        "healthcheck"
+      ];
+    };
+    # healthcheckユーザーの場合はsilentで実行
+    unix-silent = {
+      inherit (config.security.pam.services.runuser.rules.session.unix) order;
+      control = "optional";
+      modulePath = "${pkgs.pam}/lib/security/pam_unix.so";
+      settings.silent = true;
+    };
+  };
 }

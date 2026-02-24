33197680f3252e3e4d698a51731608a7f16b96f4
ncaq <ncaq@ncaq.net>
2026-02-24 16:05:36 +0900

    refactor(mackerel): services.mackerel-agentの定義順を変更
    トップダウンにします。

--- nixos/host/seminar/mackerel.nix
+++ nixos/host/seminar/mackerel.nix
@@ -3,6 +3,18 @@
   ...
 }:
 {
+  services.mackerel-agent = {
+    enable = true;
+    runAsRoot = true;
+    apiKeyFile = config.sops.templates."mackerel-api-key.conf".path;
+    settings = {
+      # エージェント自身のメモリ使用量も収集
+      diagnostic = true;
+      # ファイルシステムをデバイス名(dm-1等)ではなくマウントポイント名で表示
+      filesystems.use_mountpoint = true;
+    };
+  };
+
   # Mackerel APIキー(純粋なキー文字列)をsopsで管理
   sops.secrets."mackerel-api-key" = {
     sopsFile = ../../../secrets/seminar/mackerel.yaml;
@@ -18,17 +30,5 @@
       apikey = "${config.sops.placeholder."mackerel-api-key"}"
     '';
     mode = "0400";
-  };
-
-  services.mackerel-agent = {
-    enable = true;
-    runAsRoot = true;
-    apiKeyFile = config.sops.templates."mackerel-api-key.conf".path;
-    settings = {
-      # エージェント自身のメモリ使用量も収集
-      diagnostic = true;
-      # ファイルシステムをデバイス名(dm-1等)ではなくマウントポイント名で表示
-      filesystems.use_mountpoint = true;
-    };
   };
 }

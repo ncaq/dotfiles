261e3b7ea6db77ba0f9b86a20d95b3ded36007c1
ncaq <ncaq@ncaq.net>
2026-03-01 12:15:41 +0900

    feat(core): go.nixでgoplsをhome.packagesに追加
    - goplsをhome.packagesに追加しGo開発環境を強化

--- home/core/go.nix
+++ home/core/go.nix
@@ -1,11 +1,16 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
   programs.go = {
     enable = true;
     env.GOPATH = "${config.home.homeDirectory}/.go";
   };
 
-  home.sessionPath = [
-    "${config.home.homeDirectory}/.go/bin"
-  ];
+  home = {
+    packages = with pkgs; [
+      gopls
+    ];
+    sessionPath = [
+      "${config.home.homeDirectory}/.go/bin"
+    ];
+  };
 }

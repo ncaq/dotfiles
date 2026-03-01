a160f5c0bd93c8f5476000a11651a167db35038c
ncaq <ncaq@ncaq.net>
2026-03-01 12:45:48 +0900

    docs: YubiKeyの利用方針コメントを詳細化
    - GPG対応状況や利用用途についてコメントを追加
    - FIDO2やパスキー用途での利用を明記

--- nixos/native-linux/yubikey.nix
+++ nixos/native-linux/yubikey.nix
@@ -1,15 +1,16 @@
+/**
+  自分のYubiKeyは、
+  GPG対応版はファームウェアのバージョンが5.1.2で古くて楕円曲線暗号に対応しておらず、
+  BioはそもそもGPG機能がないため、
+  GPGには使えていません。
+  使えたとしてもGPGはソフトウェアで管理するほうが便利だと推測しています。
+  YubiKeyはFIDO2での暗号化解除やパスキーのために使っています。
+*/
 { pkgs, ... }:
 {
-  # YubiKeyは、
-  # GPG対応版はファームウェアのバージョンが5.1.2で古くて楕円曲線暗号に対応しておらず、
-  # BioはそもそもGPG機能がないため、
-  # GPGには使えていません。
-  # FIDO2のために使っています。
-
   services = {
     udev.packages = [ pkgs.yubikey-personalization ];
   };
-
   environment.systemPackages = with pkgs; [
     yubikey-manager # ykman コマンド - YubiKeyの状態確認やPIN管理に便利。
     yubikey-personalization # これのコマンドはあまり使いませんが、どうせudevのためにインストールされるので指定。

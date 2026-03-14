a93d11179d5ec0d3a90d771acef3beea7f2b7c9e
ncaq <ncaq@ncaq.net>
2026-03-14 16:54:35 +0900

    refactor(seminar): gocryptfsバックアップ設定をnoa-backup.nixへ分離
    - disk.nixからgocryptfs関連の設定を削除
    - noa-backup.nixにバックアップ用マウントポイントとパッケージ追加
    - 設定の責務を明確化し管理性を向上

--- nixos/host/seminar/disk.nix
+++ nixos/host/seminar/disk.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 {
   disko.devices = {
     disk = {
@@ -182,17 +182,6 @@
       };
     };
   };
-  # gocryptfsで暗号化されたバックアップディレクトリを`sudo mount /mnt/noa/backup`で対話的にパスフレーズ入力してマウントできるようにする。
-  fileSystems."/mnt/noa/backup" = {
-    device = "/mnt/noa/backup.encrypted";
-    fsType = "fuse.gocryptfs";
-    options = [
-      "noauto" # 起動時に自動マウントしない。
-      "nofail" # 失敗を許容する。
-      "allow_other" # 通常ユーザがアクセス出来るようにする。
-    ];
-  };
-  environment.systemPackages = [ pkgs.gocryptfs ];
   swapDevices = [
     {
       device = "/swap/swapfile";
@@ -226,8 +215,6 @@
     };
   };
   systemd.tmpfiles.rules = [
-    # gocryptfs backup mountpoint
-    "d /mnt/noa/backup 0755 ncaq ncaq -"
     # writebackモードを有効化
     "w /sys/block/bcache0/bcache/cache_mode - - - - writeback"
     "w /sys/block/bcache1/bcache/cache_mode - - - - writeback"
---
+++ nixos/host/seminar/noa-backup.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+{
+  # gocryptfsで暗号化されたバックアップディレクトリを、
+  # `sudo mount /mnt/noa/backup`で対話的にパスフレーズ入力してマウントできるようにする。
+  fileSystems."/mnt/noa/backup" = {
+    device = "/mnt/noa/backup.encrypted";
+    fsType = "fuse.gocryptfs";
+    options = [
+      "noauto" # 起動時に自動マウントしない。
+      "nofail" # 失敗を許容する。
+      "allow_other" # 通常ユーザがアクセス出来るようにする。
+    ];
+  };
+  environment.systemPackages = [ pkgs.gocryptfs ];
+  systemd.tmpfiles.rules = [
+    # gocryptfs backup mountpoint
+    "d /mnt/noa/backup 0755 ncaq ncaq -"
+  ];
+}

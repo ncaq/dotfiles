38a8a0491018893a40b40ebdd16d8e3abc9138e6
ncaq <ncaq@ncaq.net>
2026-04-05 12:50:37 +0900

    feat(native-linux): xsecurelockとU2F指紋認証による画面ロックを追加
    - xsecurelockをxss-lock経由で自動起動する設定を追加
    - YubiKey Bioの指紋認証を用いたFIDO2(U2F) PAM認証を有効化
    - 全ホスト共通のU2Fクレデンシャルを登録
    - xsecurelock用PAMサービスでU2F認証を有効化

---
+++ nixos/native-linux/screen-lock.nix
@@ -0,0 +1,40 @@
+{ pkgs, lib, ... }:
+let
+  # 共通originで登録したクレデンシャル。
+  # 自分の所有するマシンを、
+  # 自分の所有するYubiKeyで、
+  # 自分の指紋でロック解除できても何も困らないため、
+  # 全ホストで有効にしています。
+  # 登録コマンド: `pamu2fcfg -u ncaq -o pam://ncaq.net`
+  u2fKeys = {
+    # Device type: YubiKey Bio - FIDO Edition
+    # Serial number: 34849987
+    # Firmware version: 5.7.4
+    alice = "ncaq:+nkdBXuOuCKqJ41VEal3/kJaET23fIQzBEky8PgTKEaGfAAu7lmpvjey1Fai4cSNHZvnx7GPOWZJryfvMXZoFQ==,B7lUv5xvIO6UUhd3OMzBhlNaGCKwfHBb/aXBzxf1E1PvOI09uYq+Ot+seZhMwCUti3NDS3Ina06thkmE4NRPPw==,es256,+presence";
+  };
+in
+{
+  security.pam = {
+    # FIDO2(U2F) PAM認証。
+    u2f = {
+      enable = true;
+      # パスワードでもYubiKey指紋でもどちらでもOK。
+      # YubiKey Bioの指紋認証のようにセキュリティキー側で認証を行うことを前提にしています。
+      control = "sufficient";
+      settings = {
+        authfile = pkgs.writeText "u2f-mappings" "${lib.concatStringsSep ":" (lib.attrValues u2fKeys)}";
+        origin = "pam://ncaq.net";
+        cue = true; # "Please touch the device." を表示。
+      };
+    };
+    # xsecurelock用PAMサービスでU2F有効化。
+    services.xsecurelock.u2fAuth = true;
+  };
+
+  # xss-lock: systemdのlock-session/suspend等のイベントでロッカーを自動起動。
+  programs.xss-lock = {
+    enable = true;
+    # i3lockなどに比べてクラッシュ耐性などが多少堅牢らしいのでxsecurelockを指定。
+    lockerCommand = "${pkgs.xsecurelock}/bin/xsecurelock";
+  };
+}

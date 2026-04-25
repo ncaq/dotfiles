b58790f1f564ef8b5eb551f8586a1307fdccfb82
ncaq <ncaq@ncaq.net>
2026-04-25 16:04:58 +0900

    docs: コメントを修正して隣接する場所に展開します

--- home/core/alacritty.nix
+++ home/core/alacritty.nix
@@ -7,18 +7,28 @@
   ...
 }:
 let
-  alacrittyConfigFile = config.xdg.configFile."alacritty/alacritty.toml".source;
   inherit (osConfig.wsl) windowsAppData;
+  # UNIX環境で最優先される設定パスの場所は`$XDG_CONFIG_HOME/alacritty/alacritty.toml`です。
+  alacrittyConfigFile = config.xdg.configFile."alacritty/alacritty.toml".source;
+  # Windows環境(WSLではなく)では設定パスの場所は`%APPDATA%\alacritty\alacritty.toml`です。
   windowsAlacrittyConfigFile = "${windowsAppData}/alacritty/alacritty.toml";
 in
 {
   programs.alacritty = {
+    # WSL環境ではホストのWindowsのAlacrittyを使用したほうが良いため、
+    # 本来はWSL環境では有効にする必要はありません。
+    # しかし有効にしないと設定ファイルも生成されないため、
+    # ややこしくなります。
+    # よって全ての環境で`enable`にしています。
+    # 軽量なパッケージなのでインストールの負担にはあまりなりません。
+    # WindowsホストのAlacrittyはwingetでインストールすることを想定しています。
     enable = true;
-    package = pkgs.alacritty-graphics; # 画像表示対応版を選択。
-    # TODO: themeにmodus-vivendiを追加して設定します。
+    # 画像表示対応版を選択。
+    package = pkgs.alacritty-graphics;
+    # TODO: themeにmodus_vivendiを追加して設定します。
+    # theme = "modus_vivendi";
+    # themePackage = pkgs.alacritty-themes;
 
-    # UNIX環境で最優先されるパスは`$XDG_CONFIG_HOME/alacritty/alacritty.toml`であり、
-    # Windows環境(WSLではなくWindowsネイティブの話)では`%APPDATA%\alacritty\alacritty.toml`です。
     settings = {
       window = {
         decorations = "None"; # タイトルバーを消す。
@@ -31,10 +41,7 @@ in
         size = 12;
       };
       # Windows環境で起動したときはWSLのシェルを起動するようにします。
-      # WSLのalacrittyを起動することはあまり考慮していません。
-      # 話を単純にするために全ての環境でenableにしているだけなので。
-      # 軽量なパッケージなのでインストールの負担にもあまりなりません。
-      # Windowsのalacrittyはwingetでインストールすることを想定しています。
+      # WSLのalacrittyを起動することは考慮していません。
       terminal = lib.mkIf isWSL {
         shell = {
           program = "wsl.exe";
@@ -62,6 +69,11 @@ in
   };
 
   home.activation.deployAlacrittyConfigToWindows = lib.mkIf isWSL (
+    # WSL環境ではホストのWindows環境に設定ファイルを書き込みます。
+    # ブートストラップ問題は、
+    # これのインストール時ぐらいは、
+    # Windows Terminalとか適当なターミナルをを使えばいいので、
+    # 深刻に考えていません。
     lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       if [ -d ${lib.escapeShellArg windowsAppData} ]; then
         $DRY_RUN_CMD mkdir -p "$(dirname ${lib.escapeShellArg windowsAlacrittyConfigFile})"

14d3795705e685bda83a7a2853d74fe3a79792bf
ncaq <ncaq@ncaq.net>
2026-05-06 16:32:07 +0900

    fix: zramによるswapサイズを制限

--- nixos/native-linux/zram.nix
+++ nixos/native-linux/zram.nix
@@ -1,3 +1,12 @@
 _: {
-  zramSwap.enable = true;
+  zramSwap = {
+    enable = true;
+    # swapサイズが大きすぎるせいか、
+    # 異常にメモリを消費する動作が起きると、
+    # OOM Killerも動かずにシステム全体が固まってしまうことがあります。
+    # なのでzramにより設定されるswapサイズを制限します。
+    # `memoryPercent`か`memoryMax`のうち小さいほうがサイズになります。
+    memoryPercent = 20;
+    memoryMax = 8 * 1024 * 1024 * 1024; # 8GB
+  };
 }

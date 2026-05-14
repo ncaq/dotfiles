0e91e627fd5bd45aaf634fb5ed11a01fb7dc3853
ncaq <ncaq@ncaq.net>
2026-05-14 20:55:16 +0900

    docs(javascript): Denoは未対応でした

--- home/core/javascript.nix
+++ home/core/javascript.nix
@@ -6,8 +6,9 @@
   # 悪意あるパッケージがpublishされてから検出・取り下げされるまでの猶予を確保する目的です。
   # 各PMで設定キー名・単位が異なる点に注意。
 
+  # Denoはユーザグローバルの設定ファイルを公式にはサポートしていないので未対応です。
+
   # npm CLIの`min-release-age`(単位: 日)。
-  # denoも`~/.npmrc`の`min-release-age`を読むため一括で効果があります。
   # home-managerには`programs.npm`がないため`home.file`で直接生成します。
   home.file.".npmrc".text = ''
     min-release-age=3

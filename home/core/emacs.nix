8d1bd0b2efe75c72b3ca074f0e1f7bb2b89f1f30
ncaq <ncaq@ncaq.net>
2026-05-17 16:05:57 +0900

    docs(emacs): コメントの表現を改善

--- home/core/emacs.nix
+++ home/core/emacs.nix
@@ -36,9 +36,10 @@ in
     # Emacsの設定はEmacs Lispで行うのがDSLとして最適化されていて楽なので、
     # 基本的にNix言語ではなく`.emacs.d`に直接Emacs Lispを書いて管理します。
     # またEmacsの設定は即座に反映したいため、
-    # git cloneしたものを直接参照します。
+    # `git clone`したものを直接参照します。
     # `inputs.dot-emacs`も利用していますが、
-    # それは外部依存ライブラリを解決するためのものです。
+    # それは外部依存ライブラリを解決したり、
+    # Nixの設定をスマートに解決するためのものです。
     # Emacsの設定自体はローカルのファイルシステムで管理します。
     activation.cloneEmacsConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       if [ ! -d "${config.home.homeDirectory}/.emacs.d" ]; then

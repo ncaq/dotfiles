a3cb428b6dc34928254bd5d7bd1e720e033457c8
ncaq <ncaq@ncaq.net>
2026-05-19 13:08:26 +0900

    feat(power-management): サーバとラップトップの電源管理設定を変更
    tlpの省電力設定を多少強化します。
    
    設定をより明示化します。
    
    デスクトップは今の設定が最善だと判定したので変更なしです。

---
+++ nixos/laptop/tlp.nix
@@ -0,0 +1,33 @@
+# ラップトップの電源管理ツールのtlpの設定をします。
+# [NixOS/nixos-hardware](https://github.com/NixOS/nixos-hardware)
+# でラップトップたちはtlpをデフォルトで有効にされています。
+# しかしデフォルト設定がどれを選択しているのかイマイチわかりにくいので、
+# 念の為ユーザレベルでも設定しています。
+_: {
+  services = {
+    tlp = {
+      enable = true;
+      settings = {
+        # CPU動作モードを明示。
+        # amd-pstateを想定しています。
+        # kernelが自動で`active`を選びますが念の為設定しています。
+        CPU_DRIVER_OPMODE_ON_AC = "active";
+        CPU_DRIVER_OPMODE_ON_BAT = "active";
+
+        # ガバナーも明示。
+        # amd-pstateのactive modeでは`powersave`一択。
+        # `peformance`は無意味に発熱します。
+        # 負荷が高いときはCPUが自律的に周波数を上げるので、
+        # 性能が犠牲になることはありません。
+        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        # EPPはデフォルトでは`balance_performance`/`balance_power`になります。
+        # バッテリー時は電力を更に重視したいので、
+        # `balance_power`から`power`に変更します。
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      };
+    };
+  };
+}

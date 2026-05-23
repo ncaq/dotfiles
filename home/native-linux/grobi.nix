2c415745e7bb642a5bc3ea95d884f097d333fa0a
ncaq <ncaq@ncaq.net>
2026-05-23 15:26:11 +0900

    feat(grobi): 外部ディスプレイ接続時に自動消灯を無効化し切断時に復元するよう対応
    - ラップトップPCで外部ディスプレイ接続時、
      xsetでスクリーンセーバとDPMSによる画面消灯を無効化するコマンドを追加
    - 外部ディスプレイ切断時、
      `dpms-power.service`と`screensaver-timeout.service`を再起動して、
      自動消灯設定を復元する処理を追加
    - サービス起動時の初回にも復元コマンドが実行される可能性があるが軽量なため許容

--- home/native-linux/grobi.nix
+++ home/native-linux/grobi.nix
@@ -1,9 +1,23 @@
 { pkgs, config, ... }:
 let
   xrandr = "${pkgs.xorg.xrandr}/bin/xrandr";
+  xset = "${pkgs.xorg.xset}/bin/xset";
+  systemctl = "${pkgs.systemd}/bin/systemctl";
   dpiIs144 = [
     (xrandr + " --dpi 144")
     "echo 'Xft.dpi: 144'|${pkgs.xorg.xrdb}/bin/xrdb -merge"
+  ];
+  # ラップトップPCの外部ディスプレイへの接続中は動画視聴などを想定し、
+  # 画面の自動消灯を無効化する。
+  inhibitIdle = [
+    (xset + " s off") # スクリーンセーバを無効化。
+    (xset + " -dpms") # DPMSによる画面消灯を無効化。
+  ];
+  # 外部ディスプレイを外したときは自動消灯を元に戻す。
+  # 宣言的に定義されたサービスが設定値を持っているため、
+  # 値を二重管理せずにそれらのサービスを再起動して元の設定に復元する。
+  restoreIdle = [
+    (systemctl + " --user restart dpms-power.service screensaver-timeout.service")
   ];
 in
 {
@@ -132,11 +146,16 @@ in
           "HDMI-1"
           "eDP-1"
         ];
+        execute_after = inhibitIdle;
       }
       {
         # ラップトップ向けのfallback設定。
         name = "eDP-1";
         configure_single = "eDP-1";
+        # テレビ接続を外したら自動消灯とロックを元に戻す。
+        # PC起動時に初回として無駄にサービスが起動されるおそれがありますが、
+        # 軽いサービスなので許容します。
+        execute_after = restoreIdle;
       }
     ];
   };

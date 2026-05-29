50e2a864bba1470c859f0183a310231a1e0b417c
ncaq <ncaq@ncaq.net>
2026-05-29 16:49:08 +0900

    refactor(home/haskell): 常用しないHaskellツール4個を削除
    `alex`/`cabal2nix`/`happy`/`haskell-ci`をhome-managerのpackagesから削除しました。
    これらは普段直接呼び出すことがほぼなく、
    必要時はdevShellや`nix shell`で個別投入で十分です。
    
    `alex`と`happy`はパーサジェネレータで、
    cabalビルド時に依存解決で自動的に取得されるためグローバルに置く必要がありません。
    `cabal2nix`はCabalパッケージのNix化に使いますが、
    flake側で個別定義する方が筋が良いと判断しました。
    `haskell-ci`も常用しないため、
    必要時にプロジェクトのflakeから取得すれば十分です。
    
    なお当初の目的だったメモリ削減効果はありませんでした。
    home-managerに`ghc`や`haskell-language-server`や`cabal-install`や`hlint`が残っており、
    これらが`pkgs.haskellPackages`ツリーを引き続き引くため、
    削除前後で`homeConfigurations`の評価メモリは`2.25GiB`から変化しませんでした。
    本コミットは設定のクリーンアップとして位置付けます。
    
    `stylish-haskell`と`hpack`は自身では普段使わないものの、
    他者のOSSに貢献する際に必要となる場面があるため残しています。

--- home/core/haskell.nix
+++ home/core/haskell.nix
@@ -34,13 +34,9 @@ in
   home = {
     packages =
       (with pkgs; [
-        alex
         cabal-install
-        cabal2nix
         fourmolu
         ghc
-        happy
-        haskell-ci
         haskell-language-server
         hlint
         hpack

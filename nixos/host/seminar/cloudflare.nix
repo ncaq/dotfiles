144024b816a0a7c901692e28f4a7089f3d5b3e29
ncaq <ncaq@ncaq.net>
2026-05-31 11:07:38 +0900

    refactor(seminar/cloudflare): sops設定をグルーピング

--- nixos/host/seminar/cloudflare.nix
+++ nixos/host/seminar/cloudflare.nix
@@ -49,38 +49,40 @@ in
       };
     };
   };
-  # Cloudflare認証情報を管理。
-  sops.secrets = {
-    "cloudflare-cert" = {
-      sopsFile = ../../../secrets/seminar/cloudflare.yaml;
-      key = "cert_pem";
+  sops = {
+    # security.acmeのDNS-01チャレンジ用環境変数ファイル。
+    # lego(ACMEクライアント)がCloudflare DNS APIでTXTレコードを操作する。
+    templates."cloudflare-dns-env" = {
+      content = ''
+        CF_DNS_API_TOKEN=${config.sops.placeholder."cloudflare-dns-api-token"}
+      '';
       owner = "root";
       group = "root";
       mode = "0400";
     };
-    "cloudflare-tunnel-credentials" = {
-      sopsFile = ../../../secrets/seminar/cloudflare.yaml;
-      key = "tunnel_credentials";
-      owner = "root";
-      group = "root";
-      mode = "0400";
+    # Cloudflare認証情報を管理。
+    secrets = {
+      "cloudflare-cert" = {
+        sopsFile = ../../../secrets/seminar/cloudflare.yaml;
+        key = "cert_pem";
+        owner = "root";
+        group = "root";
+        mode = "0400";
+      };
+      "cloudflare-tunnel-credentials" = {
+        sopsFile = ../../../secrets/seminar/cloudflare.yaml;
+        key = "tunnel_credentials";
+        owner = "root";
+        group = "root";
+        mode = "0400";
+      };
+      "cloudflare-dns-api-token" = {
+        sopsFile = ../../../secrets/seminar/cloudflare.yaml;
+        key = "dns_api_token";
+        owner = "root";
+        group = "root";
+        mode = "0400";
+      };
     };
-    "cloudflare-dns-api-token" = {
-      sopsFile = ../../../secrets/seminar/cloudflare.yaml;
-      key = "dns_api_token";
-      owner = "root";
-      group = "root";
-      mode = "0400";
-    };
-  };
-  # security.acmeのDNS-01チャレンジ用環境変数ファイル。
-  # lego(ACMEクライアント)がCloudflare DNS APIでTXTレコードを操作する。
-  sops.templates."cloudflare-dns-env" = {
-    content = ''
-      CF_DNS_API_TOKEN=${config.sops.placeholder."cloudflare-dns-api-token"}
-    '';
-    owner = "root";
-    group = "root";
-    mode = "0400";
   };
 }

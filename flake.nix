fbb025d2a3561777eff46679a9a1979f60706bca
ncaq <ncaq@ncaq.net>
2026-06-01 13:37:28 +0900

    build(deps): nixpkgsとhome-managerを25.11から26.05にバージョンアップ

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "dotfiles, NixOS and home-manager.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -14,7 +14,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 

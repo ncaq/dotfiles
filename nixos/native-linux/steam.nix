6455869d31a696c7e67d99871d0e2d4928a93256
ncaq <ncaq@ncaq.net>
2026-07-12 16:41:06 +0900

    feat: SteamとSteamOS互換のゲーム環境をセットアップ
    Remote Playでラップトップから大画面でゲームを遊べるようにするため、
    Steam本体とgamescopeとgamemodeを導入します。
    
    ホスト個別ではなく共有設定にしたいので`nixos/native-linux/`に配置します。
    seminarにもインストールされますが、
    GUIを起動しないサーバなので実害はありません。
    
    steamのライセンスは`unfreeRedistributable`で、
    既存の`allowlistedLicenses`に含まれているため、
    unfree許可の変更は不要でした。
    
    LightDMがgamescopeのWaylandセッションを起動できるかは未検証なので、
    `gamescopeSession`を有効にして実機で試します。
    
    `programs.gamescope.capSysNice`は、
    Steamのpressure-vessel内でcapability継承に失敗する既知問題があるため設定しません。
    https://github.com/NixOS/nixpkgs/issues/351516
    
    close #1316

---
+++ nixos/native-linux/steam.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+{
+  programs = {
+    steam = {
+      enable = true;
+      # Remote Playのホスト・クライアント通信用にポートを開放。
+      remotePlay.openFirewall = true;
+      # ローカルネットワーク経由のゲームファイル転送用にポートを開放。
+      localNetworkGameTransfers.openFirewall = true;
+      # SteamOS風のgamescopeセッションを選べるようにする。
+      gamescopeSession.enable = true;
+      # 互換性問題のあるゲーム向けにProton-GEを導入。
+      extraCompatPackages = with pkgs; [ proton-ge-bin ];
+      # SteamのFHS環境内で`mangohud %command%`を使えるようにする。
+      extraPackages = with pkgs; [ mangohud ];
+      # Protonプレフィックスへwinetricksを適用するトラブルシューティングツール。
+      protontricks.enable = true;
+    };
+    # `gamescopeSession`が有効化するが、
+    # 単体ゲームのネスト起動にも使うので明示。
+    gamemode.enable = true;
+    gamescope.enable = true;
+  };
+}

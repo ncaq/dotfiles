99f33353b7ba6ff1f2c965bfd8a61224e552fd5c
ncaq <ncaq@ncaq.net>
2026-07-12 17:41:12 +0900

    feat: gamescopeセッションをTTY4へのログインで起動できるように設定
    LightDMはWaylandセッションを起動できないため、
    `gamescopeSession`が登録するセッションをログイン画面から選ぶことはできません。
    かといってX11上のネスト起動ではHDRなどを通せません。
    
    そこで特定のttyにログインすると`steam-gamescope`を`exec`するようにして、
    埋め込み(DRM)モードのgamescopeセッションを起動できるようにします。
    logindセッション経由でDRMマスターを取得するので、
    ディスプレイマネージャなしでもフルスクリーンで立ち上がります。
    gamescopeを終了するとログインプロンプトに戻り、
    X11のセッションは別VTでそのまま並存します。

--- nixos/native-linux/steam.nix
+++ nixos/native-linux/steam.nix
@@ -7,7 +7,8 @@
       remotePlay.openFirewall = true;
       # ローカルネットワーク経由のゲームファイル転送用にポートを開放。
       localNetworkGameTransfers.openFirewall = true;
-      # SteamOS風のgamescopeセッションを選べるようにする。
+      # LightDMはこれが登録するWaylandセッションを起動できないが、
+      # TTYからの起動に使う`steam-gamescope`コマンドの提供元なので有効にしておく。
       gamescopeSession.enable = true;
       # 互換性問題のあるゲーム向けにProton-GEを導入。
       extraCompatPackages = with pkgs; [ proton-ge-bin ];
@@ -21,4 +22,16 @@
     gamemode.enable = true;
     gamescope.enable = true;
   };
+  # LightDMはWaylandセッションを起動できず、
+  # X11上のネスト起動ではHDRなどを通せないため、
+  # 埋め込み(DRM)モードのgamescopeセッションは特定のttyへのログインで起動する。
+  # tty4でログインするとlogindセッション経由でDRMマスターを取得して、
+  # SteamOS風のフルスクリーンUIが立ち上がる。
+  # 終了するとログインプロンプトに戻る。
+  # X11のセッションは別VTでそのまま並存する。
+  environment.loginShellInit = ''
+    if [ "$(tty)" = "/dev/tty4" ]; then
+      exec steam-gamescope
+    fi
+  '';
 }

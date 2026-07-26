d1ed135958b5c658db57ecaade767e24211f4ae7
ncaq <ncaq@ncaq.net>
2026-07-26 11:49:44 +0900

    feat: creepのbeesのメモリ消費量を抑える
    creepは搭載メモリが少ないので抑制的に設定します。
    もともとSSDにそんなに多くのデータを置く作業をするわけではないので、
    beesの需要もそこまで多くない。

--- nixos/host/creep/bees.nix
+++ nixos/host/creep/bees.nix
@@ -4,10 +4,9 @@
   # CPUとI/Oの優先度は元々アイドル相当。
   services.beesd.filesystems.root = {
     spec = "/";
-    # 検出粒度よりメモリ消費を優先して1GBに抑える。
-    # 約692GiBのSSD全域に対しては約11KB粒度で、
-    # それ未満の細かい重複は見逃す。
-    hashTableSizeMB = 1024;
+    # 検出粒度よりメモリ消費を優先して抑える。
+    # 細かい重複は見逃す。
+    hashTableSizeMB = 512;
     extraOptions = [
       # 他プロセスが動き出したら縮退。
       # ラップトップなので最も保守的に、

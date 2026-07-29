6efcdfb13e12531fcafb21825af912be2607e9a8
ncaq <ncaq@ncaq.net>
2026-07-29 10:48:02 +0900

    refactor: revに分かりやすい方のバージョンを採用

--- nixos/host/bullet/comfyui/custom-node.nix
+++ nixos/host/bullet/comfyui/custom-node.nix
@@ -28,7 +28,7 @@ in
       "ComfyUI-SeedVR2_VideoUpscaler" = pkgs.fetchFromGitHub {
         owner = "numz";
         repo = "ComfyUI-SeedVR2_VideoUpscaler";
-        rev = "4490bd1f482e026674543386bb2a4d176da245b9";
+        rev = "2.5.24";
         hash = "sha256-6nsqFflLw9vYH/du35ET46fdAm1NMjjTe2bA8JmaBE4=";
       };
       # UltralyticsDetectorProvider(YOLOによる顔検出)を提供する。

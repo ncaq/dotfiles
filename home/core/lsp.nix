143c3bcae15795edaf755aef3293777fa285d0d5
ncaq <ncaq@ncaq.net>
2026-08-05 13:41:27 +0900

    feat(home): Language Serverの対応範囲を拡大します
    Emacs設定で利用しているLanguage Serverを共通の`lsp.nix`にも追加します。
    テキストエディタやコーディングエージェントから同じサーバ群を利用可能にします。

--- home/core/lsp.nix
+++ home/core/lsp.nix
@@ -1,21 +1,50 @@
 { pkgs, ... }:
 {
+  # 様々なツールで利用するlspサーバをインストールします。
+  # Emacsなどのテキストエディタや、
+  # コーディングエージェントなどが利用します。
   home.packages = with pkgs; [
+    bash-language-server
     ccls
     clang-tools
+    clojure-lsp
+    cmake-language-server
     csharp-ls
+    dhall-lsp-server
     docker-compose-language-service
     dockerfile-language-server
+    elixir-ls
+    elmPackages.elm-language-server
+    erlang-language-platform
+    fortls
     gopls
+    graphql-language-service-cli
     haskell-language-server
     jdt-language-server
     kotlin-language-server
+    ltex-ls-plus
     lua-language-server
     marksman
+    metals
+    nginx-language-server
     nil
+    ocamlPackages.ocaml-lsp
+    omnisharp-roslyn
+    pyright
     ruby-lsp
+    rust-analyzer
+    serve-d
     sourcekit-lsp
+    sqls
+    svelte-language-server
+    tailwindcss-language-server
+    taplo
+    terraform-ls
+    texlab
     typescript-language-server
     vscode-langservers-extracted
+    vue-language-server
+    yaml-language-server
+    zls
   ];
 }

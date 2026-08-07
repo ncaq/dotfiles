5bd556ea384a8e5a179037c66ef600ace963af12
ncaq <ncaq@ncaq.net>
2026-08-07 18:34:16 +0900

    docs: コメント位置を合わせる

--- home/core/emacs.nix
+++ home/core/emacs.nix
@@ -49,20 +49,18 @@ in
     defaultEditor = true;
   };
 
-  home = {
-    # Emacsの設定はEmacs Lispで行うのがDSLとして最適化されていて楽なので、
-    # 基本的にNix言語ではなく`.emacs.d`に直接Emacs Lispを書いて管理します。
-    # またEmacsの設定は即座に反映したいため、
-    # `git clone`したものを直接参照します。
-    # `inputs.dot-emacs`も利用していますが、
-    # それは外部依存ライブラリを解決したり、
-    # Nixの設定をスマートに解決するためのものです。
-    # Emacsの設定自体はローカルのファイルシステムで管理します。
-    packages = [ emacsPackage ];
-  };
-
+  # Emacsの設定はEmacs Lispで行うのがDSLとして最適化されていて楽なので、
+  # 基本的にNix言語ではなく`.emacs.d`に直接Emacs Lispを書いて管理します。
+  # またEmacsの設定は即座に反映したいため、
+  # `git clone`したものを直接参照します。
+  # `inputs.dot-emacs`も利用していますが、
+  # それは外部依存ライブラリを解決したり、
+  # Nixの設定をスマートに解決するためのものです。
+  # Emacsの設定自体はローカルのファイルシステムで管理します。
   programs.git-repo-subscribe.repositories.dot-emacs = {
     url = "https://github.com/ncaq/.emacs.d.git";
     path = "${config.home.homeDirectory}/.emacs.d";
   };
+
+  home.packages = [ emacsPackage ];
 }
